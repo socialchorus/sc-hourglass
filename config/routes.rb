abd4b6091a22a5b86b0317e3936284838308171d
socialchorus <developers@socialchorus.com>
2013-10-02 19:05:26 -0700

    changes root to events/index

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Hourglass::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'events#index'
 
   # See how all your routes lay out with "rake routes"
 
